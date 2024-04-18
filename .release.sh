e1757a85a9a4416ea2781a3f333e94e25ec0d5f0
Karel Kočí <cynerd@email.cz>
2024-04-18 13:23:55 +0200

    gitlab-ci: update and extend used checks
    This also reworks flake.nix a bit to pass statix and somewhat deadnix.
    
    This also removes distribution archives so everyone need to use either
    Gitlab generated snapshot of repository or git directly. This is
    reaction on attack of xz distribution archives. We do not have to ship
    them anyway. There is a little to no incentive to do so right now.

--- .release.sh
+++ .release.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 set -eu
 
+semregexp='^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$'
 version="${CI_COMMIT_TAG#v}"
-if ! grep -qE '^[0-9]+\.[0-9]+\.[0-9]+$' <<<"$version"; then
-	echo "Tag has to be valid version number such as v1.0.0!" >&2
+if ! grep -qE "$semregexp" <<<"$version"; then
+	echo "Tag has to be valid semantic version!" >&2
+	exit 1
+fi
+
+if [[ "$(cat version)" != "$version" ]]; then
+	echo "Version file does not contain the correct version!" >&2
 	exit 1
 fi
 
@@ -28,23 +34,12 @@ changelog="$(awk -v "version=$version" '
 		}
 	' CHANGELOG.md)"
 if [ -z "$changelog" ]; then
-	echo "Changelog is empty. Have you updated the version? Is this the latest version?" >&2
+	echo "Changelog is empty." \
+		"Have you updated the version? Is this the latest version?" >&2
 	exit 1
 fi
-
-declare -a args
-while read -r dists; do
-	for dist in $dists; do
-		[ -f "$dist" ] || continue # ignore missing / not built
-		url="${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/generic/release/${version}/${dist##*/}"
-		echo "$dist -> $url"
-		curl --header "JOB-TOKEN: ${CI_JOB_TOKEN}" --upload-file "${dist}" "${url}"
-		args+=("--assets-link" "{\"name\":\"${dist}\",\"url\":\"${url}\"}")
-	done
-done <<<"$(yq '.build.artifacts.paths[]' .gitlab-ci.yml)"
 
 release-cli create \
 	--name "Release $version" \
 	--tag-name "$CI_COMMIT_TAG" \
-	--description "$changelog" \
-	"${args[@]}"
+	--description "$changelog"
