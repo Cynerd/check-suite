a47e38f17c249efeb7819d61d98b2b0c91bde205
Karel Kočí <cynerd@email.cz>
2024-04-18 14:38:00 +0200

    Check Suite version 1.0.2
    Changed
     * `array` is now optional for `ARRAY_TEST*` and deduced from the test
       name if not specified

--- .release.sh
+++ .release.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 set -eu
 
-semregexp='^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$'
 version="${CI_COMMIT_TAG#v}"
-if ! grep -qE "$semregexp" <<<"$version"; then
+if ! pysemver check "$version"; then
 	echo "Tag has to be valid semantic version!" >&2
-	exit 1
-fi
-
-if [[ "$(cat version)" != "$version" ]]; then
-	echo "Version file does not contain the correct version!" >&2
 	exit 1
 fi
 
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,7 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Unreleased]
+## [1.0.2] - 2024-04-18
 # Changed
 - `array` is now optional for `ARRAY_TEST*` and deduced from the test name if
   not specified
